0478eb8254a1449287f24750dc06e88f64f4b1b3
Marcos <125505999+marcosscruz@users.noreply.github.com>
2023-06-10 20:03:44 -0300

    reimplementando
    irei tentar outra forma de implementar as classes

---
+++ Sistema Pousada/src/br/com/pousada/servicos/Quarto.java
@@ -0,0 +1,12 @@
+package br.com.pousada.servicos;
+
+/**
+ *
+ * Classe representativa da entidade Quarto no Sistema
+ *
+ * @author Marcos Vinícius Santos Cruz
+ * @author Filipe
+ */
+public class Quarto {
+    
+}
---
+++ Sistema Pousada/src/br/com/pousada/servicos/Reserva.java
@@ -0,0 +1,17 @@
+package br.com.pousada.servicos;
+
+import java.util.*;
+import java.time.format.*;
+import java.util.ArrayList;
+
+/**
+ * Classe representativa da entidade Reserva no Sistema
+ *
+ * @author Marcos Vinícius Santos Cruz
+ * @author Filipe
+ */
+// Q.1 - Implementar todas as classes com base no diagrama de classes criado
+public class Reserva {
+    
+    
+}
