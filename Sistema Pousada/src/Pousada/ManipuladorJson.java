cb92a192ef00d28c72daa1aaad3d43761c84d50c
Marcos <125505999+marcosscruz@users.noreply.github.com>
2023-06-10 20:00:13 -0300

    Create ManipuladorJson.java
    manipular a estrutura de dados do JSON

---
+++ Sistema Pousada/src/Pousada/ManipuladorJson.java
@@ -0,0 +1,15 @@
+package Pousada;
+
+/**
+ *
+ * Classe que contém métodos de manipulação de arquivos JSON
+ * 
+ * @author Marcos Vinícius Santos Cruz
+ * @author Filipe
+ */
+
+// Q.1 - Implementar todas as classes com base no diagrama de classes cirado
+// Q.13 - Salve e recuper
+public class ManipuladorJson {
+    
+}
