5e3b33bf0c9c9aa78d25032827b8f7d4a536809b
Marcos <marcos.v.s.cruz10@gmail.com>
2023-06-14 11:22:30 -0300

    Update Hospede.java
    algumas alterações em relação ao contador de instâncias de Hóspede

--- Sistema Pousada/src/br/com/pousada/pessoas/Hospede.java
+++ Sistema Pousada/src/br/com/pousada/pessoas/Hospede.java
@@ -19,42 +19,29 @@ public class Hospede extends Pessoa {
     private ArrayList<Reserva> reservasHospede = new ArrayList<>();
 
     /**
-     * Q.11 - Criar duas variáveis de classe (static) que irão armazenar quantas
-     * instâncias foram criadas dos tipos Cliente dentro da classe Sistema
-     * usando duas soluções diferentes:
-     *
-     * a. Uma delas utilizando o enfoque de encapsulamento de acordo com a
-     * engenharia de software (atributo private static e métodos get e set);
-     *
-     * b. Na segunda estratégia, implementar usando o controle de acesso do tipo
-     * protect;
-     */
-    private static int hospedeCount1;
-    private static int hospedeCount2;
-
-    /**
      * Construtor parametrizado para o obejeto Hospede
      * Realiza uma chamada ao construtor parametrizado da classe pai (Pessoa)
      * 
-     * @param nomePessoa define um nome para o Hóspede
-     * @param sobrenomePessoa define um sobrenome para o Hóspede 
-     * @param CPF define um número de CPF para o Hóspede
+     * @param nomePessoa      define um nome para o Hóspede
+     * @param sobrenomePessoa define um sobrenome para o Hóspede
+     * @param CPF             define um número de CPF para o Hóspede
      * @param enderecoHospede define um endereço para o Hóspede
      * @param telefoneHospede define um telefone de contato para o Hóspede
-     * @param emailHospde define um email de contato para o Hóspede
+     * @param emailHospde     define um email de contato para o Hóspede
      */
     // Q.4 - Utilizar a palavra chave super para implementar os construtores das subsclasses
-    public Hospede(String nomePessoa, String sobrenomePessoa, String CPF, String enderecoHospede, String telefoneHospede, String emailHospde) {
+    public Hospede(String nomePessoa, String sobrenomePessoa, String CPF, String enderecoHospede,
+            String telefoneHospede, String emailHospde) {
         super(nomePessoa, sobrenomePessoa, CPF);
         this.enderecoHospede = enderecoHospede;
         this.telefoneHospede = telefoneHospede;
         this.emailHospede = emailHospde;
-        setHospedeCount1(hospedeCount1);
-        setHospedeCount2(hospedeCount2);
     }
 
-    // Construtor padrão realiza uma chamada ao construtor padrão da classe pai (Pessoa)
-    // Q.4 - Utilizar a palavra chave super para implementar os construtores das subsclasses
+    // Construtor padrão realiza uma chamada ao construtor padrão da classe pai
+    // (Pessoa)
+    // Q.4 - Utilizar a palavra chave super para implementar os construtores das
+    // subsclasses
     public Hospede() {
         super();
     }
@@ -84,36 +71,19 @@ public class Hospede extends Pessoa {
         this.emailHospede = emailHospede;
     }
 
-    public ArrayList<Reserva> getReservasHospede(){
+    public ArrayList<Reserva> getReservasHospede() {
         return reservasHospede;
     }
 
-    public void setReservasHospede(Reserva novaReserva){
+    public void setReservasHospede(Reserva novaReserva) {
         this.reservasHospede.add(0, novaReserva);
-    }
-
-    // Q.11 - a. Utilizando o enfoque de encapsulamento de acordo com a engenharia de software
-    public static int getHospedeCount1() {
-        return hospedeCount1;
-    }
-
-    public static void setHospedeCount1(int hospedeCount1) {
-        Hospede.hospedeCount1 = hospedeCount1 + 1;
-    }
-
-    // Q.11 - b. Usando o controle de acesso do tipo protect
-    protected static int getHospedeCount2() {
-        return hospedeCount2;
-    }
-
-    protected static void setHospedeCount2(int hospedeCount2) {
-        Hospede.hospedeCount2 = hospedeCount2 + 1;
     }
 
     // Q.3 - Sobrescrever o método toString() de todas as classes implementadas
     @Override
     public String toString() {
-        return getCPF() + " " + getNomePessoa().toUpperCase() + "   " + getSobrenomePessoa().toUpperCase() + "  " + getEnderecoHospede()
+        return getCPF() + " " + getNomePessoa().toUpperCase() + "   " + getSobrenomePessoa().toUpperCase() + "  "
+                + getEnderecoHospede()
                 + "    " + getTelefoneHospede() + "    " + getEmailHospede();
     }
 }
