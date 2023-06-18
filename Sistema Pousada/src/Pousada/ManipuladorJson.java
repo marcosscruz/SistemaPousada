1e6c9eb82f997fbceaca04d5a0999d50e176ec72
Marcela <marcos.v.s.cruz10@gmail.com>
2023-06-17 21:58:47 -0300

    Update ManipuladorJson.java

--- Sistema Pousada/src/Pousada/ManipuladorJson.java
+++ Sistema Pousada/src/Pousada/ManipuladorJson.java
@@ -1,17 +1,20 @@
 package Pousada;
 
 /**
- *
  * Classe que contém métodos de manipulação de arquivos JSON
  * 
  * @author Marcos Vinícius Santos Cruz
  * @author Filipe
  */
 
-
 /**
  * Q.1 - Implementar todas as classes com base no diagrama de classes cirado
- * Q.14 - Salve e recupere todas as informações dos Clientes, Quartos, Colaboradores e Reservas em um arquivo de texto.
+ * 
+ * Q.14 - Salve e recupere todas as informações dos Clientes, Quartos,
+ * Colaboradores e Reservas em um arquivo de texto. Utilizem classes já prontas
+ * na internet que trabalhem com o formato json. Ao manipular um arquivo utilize
+ * os conceitos aprendidos em aula para alocar e desalocar recursos com
+ * segurança.
  */
 public class ManipuladorJson {
 
