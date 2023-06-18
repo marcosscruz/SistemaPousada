f488106895b37d3d23bfbe468ecd4cf5cc7aa8b8
Marcela <marcos.v.s.cruz10@gmail.com>
2023-06-17 21:57:34 -0300

    Create MenuSistema.java
    classe que será responsável por exibir quais as opções de acordo com o usuário atual

---
+++ Sistema Pousada/src/Pousada/MenuSistema.java
@@ -0,0 +1,26 @@
+package Pousada;
+
+import java.io.IOException;
+
+import br.com.pousada.pessoas.Usuario;
+import br.com.pousada.servicos.GerenciadorAdm;
+
+/**
+ * Classe representativa do Menu de Opções do Sistema
+ * 
+ * @author Marcos Vinícius Santos Cruz
+ * @author Filipe
+ */
+// Q.1 - Implementar todas as classes com base no diagrama de classes criado
+public class MenuSistema {
+
+    /**
+     * Construtor padrão
+     */
+    public MenuSistema() {
+    }
+
+    public void menuAdministrador(GerenciadorAdm menuAdm, Usuario userAtual) throws IOException {
+
+    }
+}
